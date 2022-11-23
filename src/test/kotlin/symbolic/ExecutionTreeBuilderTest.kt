28a52438aea27058cc100345276d16ec76d0e8d5
Gabriele Pappalardo <leleattolo99@gmail.com>
2022-11-23 20:42:43 +0100

    Add tests for new comparison operators

--- src/test/kotlin/symbolic/ExecutionTreeBuilderTest.kt
+++ src/test/kotlin/symbolic/ExecutionTreeBuilderTest.kt
@@ -27,7 +27,70 @@ internal class ExecutionTreeBuilderTest {
         )
 
         val tree = ExecutionTreeBuilder.astToExecutionTree(fooBarAst)
+        println(GraphvizVisualizer.drawTree(tree))
+    }
 
+    @Test
+    fun testGreaterThan() {
+        val ast = Expr.Block(
+            Expr.Let(Expr.Var("a"), Expr.SymVal("α")),
+            Expr.Let(Expr.Var("b"), Expr.SymVal("β")),
+            Expr.Let(Expr.Var("x"), Expr.Const(0)),
+            Expr.Let(Expr.Var("y"), Expr.Const(0)),
+            Expr.If(Expr.Comparison.Gt(Expr.Var("a"), Expr.Var("b")),
+                Expr.Let(Expr.Var("x"), Expr.Const(40)),
+                Expr.Let(Expr.Var("y"), Expr.Const(2))),
+            Expr.Plus(Expr.Var("x"), Expr.Var("y"))
+        )
+        val tree = ExecutionTreeBuilder.astToExecutionTree(ast)
+        println(GraphvizVisualizer.drawTree(tree))
+    }
+
+    @Test
+    fun testGreaterOrEqualThan() {
+        val ast = Expr.Block(
+            Expr.Let(Expr.Var("a"), Expr.SymVal("α")),
+            Expr.Let(Expr.Var("b"), Expr.SymVal("β")),
+            Expr.Let(Expr.Var("x"), Expr.Const(0)),
+            Expr.Let(Expr.Var("y"), Expr.Const(0)),
+            Expr.If(Expr.Comparison.Gte(Expr.Var("a"), Expr.Var("b")),
+                Expr.Let(Expr.Var("x"), Expr.Const(40)),
+                Expr.Let(Expr.Var("y"), Expr.Const(2))),
+            Expr.Plus(Expr.Var("x"), Expr.Var("y"))
+        )
+        val tree = ExecutionTreeBuilder.astToExecutionTree(ast)
+        println(GraphvizVisualizer.drawTree(tree))
+    }
+
+    @Test
+    fun testLessThan() {
+        val ast = Expr.Block(
+            Expr.Let(Expr.Var("a"), Expr.SymVal("α")),
+            Expr.Let(Expr.Var("b"), Expr.SymVal("β")),
+            Expr.Let(Expr.Var("x"), Expr.Const(0)),
+            Expr.Let(Expr.Var("y"), Expr.Const(0)),
+            Expr.If(Expr.Comparison.Lt(Expr.Var("a"), Expr.Var("b")),
+                Expr.Let(Expr.Var("x"), Expr.Const(40)),
+                Expr.Let(Expr.Var("y"), Expr.Const(2))),
+            Expr.Plus(Expr.Var("x"), Expr.Var("y"))
+        )
+        val tree = ExecutionTreeBuilder.astToExecutionTree(ast)
+        println(GraphvizVisualizer.drawTree(tree))
+    }
+
+    @Test
+    fun testLessOrEqualThan() {
+        val ast = Expr.Block(
+            Expr.Let(Expr.Var("a"), Expr.SymVal("α")),
+            Expr.Let(Expr.Var("b"), Expr.SymVal("β")),
+            Expr.Let(Expr.Var("x"), Expr.Const(0)),
+            Expr.Let(Expr.Var("y"), Expr.Const(0)),
+            Expr.If(Expr.Comparison.Lte(Expr.Var("a"), Expr.Var("b")),
+                Expr.Let(Expr.Var("x"), Expr.Const(40)),
+                Expr.Let(Expr.Var("y"), Expr.Const(2))),
+            Expr.Plus(Expr.Var("x"), Expr.Var("y"))
+        )
+        val tree = ExecutionTreeBuilder.astToExecutionTree(ast)
         println(GraphvizVisualizer.drawTree(tree))
     }
 
